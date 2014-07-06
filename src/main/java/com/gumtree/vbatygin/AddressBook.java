4134ce24a7ed1a97326ae94761cc844505e1f7c8
vbatygin <botdes@gmail.com>
2014-07-06 18:24:01 +0100

    implemented function howManyDaysOlder

--- src/main/java/com/gumtree/vbatygin/AddressBook.java
+++ src/main/java/com/gumtree/vbatygin/AddressBook.java
@@ -1,34 +1,55 @@
 package com.gumtree.vbatygin;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 public class AddressBook {
 
     private int maleCount = 0;
     private int femaleCount = 0;
     private Person oldest = null;
+    private final Map<String, Date> nameToDayOfBirth = new HashMap<String, Date>();
 
     public int countByGender(Gender gender) {
-       return gender == Gender.MALE ? maleCount : femaleCount;
+        return gender == Gender.MALE ? maleCount : femaleCount;
     }
 
     public Person getOldestPerson() {
         return oldest;
     }
 
-    public int howManyDaysOlder(String firstPerson, String secondPerson) {
-        throw new UnsupportedOperationException();
+    public long howManyDaysOlder(String firstPerson, String secondPerson) {
+        final Date firstDay = getDate(firstPerson);
+        final Date secondDay = getDate(secondPerson);
+        return TimeUnit.MILLISECONDS.toDays(secondDay.getTime() - firstDay.getTime());
+    }
+
+    private Date getDate(final String person) {
+        final Date date = nameToDayOfBirth.get(person);
+        if (date == null) {
+            throw new IllegalArgumentException("person do not exist in address book " + person);
+        }
+        return date;
     }
 
     public void add(final Person person) {
+        if (person == null) {
+            throw new IllegalArgumentException("person must not be null");
+        }
+        if (nameToDayOfBirth.containsKey(person.getName())) {
+            throw new IllegalArgumentException("person with name " + person.getName() + " already exist in addressBook");
+        }
         if (person.getGender() == Gender.MALE) {
             maleCount++;
         } else {
-            femaleCount ++;
+            femaleCount++;
         }
 
         if (oldest == null || person.getDayOfBirth().before(oldest.getDayOfBirth())) {
             oldest = person;
         }
-
-
+        nameToDayOfBirth.put(person.getName(), person.getDayOfBirth());
     }
 }
--- src/test/java/com/gumtree/vbatygin/AddressBookTest.java
+++ src/test/java/com/gumtree/vbatygin/AddressBookTest.java
@@ -5,11 +5,16 @@ import org.junit.Test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 public class AddressBookTest {
     private final SimpleDateFormat format = new SimpleDateFormat("dd/mm/yyyy");
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddNull() {
+        final AddressBook addressBook = new AddressBook();
+        addressBook.add(null);
+    }
 
     @Test
     public void testMaleCountEmptyBook() {
@@ -35,10 +40,56 @@ public class AddressBookTest {
     @Test
     public void testGetOldest() throws ParseException {
         final AddressBook addressBook = new AddressBook();
-         addressBook.add(new Person("test", Gender.MALE, format.parse("20/01/1990")));
+        addressBook.add(new Person("test", Gender.MALE, format.parse("20/01/1990")));
         addressBook.add(new Person("test2", Gender.MALE, format.parse("20/01/1931")));
         addressBook.add(new Person("test3", Gender.FEMALE, format.parse("20/02/1931")));
         addressBook.add(new Person("test4", Gender.MALE, format.parse("20/01/1931")));
         Assert.assertEquals("test2", addressBook.getOldestPerson().getName());
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddEqualNames() {
+        final AddressBook addressBook = new AddressBook();
+        addressBook.add(new Person("test", Gender.MALE, new Date()));
+        addressBook.add(new Person("test", Gender.FEMALE, new Date()));
+    }
+
+    @Test
+    public void testHowManyDaysOlder() throws ParseException {
+        final AddressBook addressBook = new AddressBook();
+        addressBook.add(new Person("test", Gender.MALE, format.parse("20/01/1990")));
+        addressBook.add(new Person("test2", Gender.MALE, format.parse("20/01/1991")));
+        Assert.assertEquals(365, addressBook.howManyDaysOlder("test", "test2"));
+    }
+
+    @Test
+    public void testHowManyDaysOlderReversed() throws ParseException {
+        final AddressBook addressBook = new AddressBook();
+        addressBook.add(new Person("test", Gender.MALE, format.parse("20/01/1990")));
+        addressBook.add(new Person("test2", Gender.MALE, format.parse("20/01/1991")));
+        Assert.assertEquals(-365, addressBook.howManyDaysOlder("test2", "test"));
+    }
+
+    @Test
+    public void testHowManyDaysOlderEqual() throws ParseException {
+        final AddressBook addressBook = new AddressBook();
+        addressBook.add(new Person("test", Gender.MALE, format.parse("20/01/1991")));
+        addressBook.add(new Person("test2", Gender.MALE, format.parse("20/01/1991")));
+        Assert.assertEquals(0, addressBook.howManyDaysOlder("test2", "test"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testHowManyDaysNonexistentFirst() throws ParseException {
+        final AddressBook addressBook = new AddressBook();
+        addressBook.add(new Person("test", Gender.MALE, format.parse("20/01/1991")));
+        addressBook.howManyDaysOlder("stranger", "test");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testHowManyDaysNonexistentSecond() throws ParseException {
+        final AddressBook addressBook = new AddressBook();
+        addressBook.add(new Person("test", Gender.MALE, format.parse("20/01/1991")));
+        addressBook.howManyDaysOlder("test", "stranger");
+    }
+
 }
